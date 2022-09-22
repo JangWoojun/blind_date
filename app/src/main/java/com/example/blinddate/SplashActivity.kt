4ce49f9ab2cea0678764470225c2093c1b66e87d
JangWoojun <woojun0107@naver.com>
2022-09-22 15:51:43 +0900

    2022/09/22 Intro화면 오류 수정

--- app/src/main/java/com/example/blinddate/SplashActivity.kt
+++ app/src/main/java/com/example/blinddate/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.example.blinddate.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, IntroActivity::class.java))
             finish()
         },3000)
     }
--- app/src/main/java/com/example/blinddate/auth/IntroActivity.kt
+++ app/src/main/java/com/example/blinddate/auth/IntroActivity.kt
@@ -1,12 +1,26 @@
 package com.example.blinddate.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.blinddate.R
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        val joinBtn = findViewById<Button>(R.id.JoinBtn)
+        joinBtn.setOnClickListener {
+            val intent = Intent(this,JoinActivity::class.java)
+            startActivity(intent)
+        }
+        val loginBtn = findViewById<Button>(R.id.LoginBtn)
+        loginBtn.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
