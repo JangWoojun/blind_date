c6890fca7718fbb7131a26ba329fc8966080fb4d
JangWoojun <woojun0107@naver.com>
2022-09-24 15:02:41 +0900

    2022/09/24 로그인 기능 구현

--- app/src/main/java/com/example/blinddate/auth/LoginActivity.kt
+++ app/src/main/java/com/example/blinddate/auth/LoginActivity.kt
@@ -1,12 +1,43 @@
 package com.example.blinddate.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.blinddate.MainActivity
 import com.example.blinddate.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        loginBtn.setOnClickListener {
+
+            val email = findViewById<TextInputEditText>(R.id.emailArea).text.toString()
+            val password = findViewById<TextInputEditText>(R.id.passwordArea).text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this,"로그인을 성공하셨습니다", Toast.LENGTH_LONG).show()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else {
+                        Toast.makeText(this,"로그인을 실패하셨습니다", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
     }
 }
