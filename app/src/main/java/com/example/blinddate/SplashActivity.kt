10e47d4f8d19ea94f019f1e62b8d3b83176e8d7c
JangWoojun <woojun0107@naver.com>
2022-09-24 14:46:54 +0900

    2022/09/24 로그인 되어있는 유저인지 판단해서 필요한 Activity 이동 하는 기능 구현

--- app/src/main/java/com/example/blinddate/SplashActivity.kt
+++ app/src/main/java/com/example/blinddate/SplashActivity.kt
@@ -5,15 +5,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.blinddate.auth.IntroActivity
+import com.example.blinddate.utils.FirebaseAuthUtils
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
-        },3000)
+        val uid = FirebaseAuthUtils.getUid()
+
+        if (uid != "null"){
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            },3000)
+        }
+        else {
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+            },3000)
+        }
+
+
     }
 }
