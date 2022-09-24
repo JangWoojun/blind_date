db3e25a3707aa64d7b269ba8af9cb9173ea3a5c1
JangWoojun <woojun0107@naver.com>
2022-09-24 14:09:41 +0900

    2022/09/24 이메일,비밀번호 입력 받아오기 구현

--- app/src/main/java/com/example/blinddate/auth/JoinActivity.kt
+++ app/src/main/java/com/example/blinddate/auth/JoinActivity.kt
@@ -2,11 +2,26 @@ package com.example.blinddate.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import com.example.blinddate.R
+import com.google.android.material.textfield.TextInputEditText
 
 class JoinActivity : AppCompatActivity() {
+
+    private val TAG = "JoinActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        val joinBtn = findViewById<Button>(R.id.joinBtn)
+        joinBtn.setOnClickListener {
+            val email = findViewById<TextInputEditText>(R.id.emailArea)
+            val password = findViewById<TextInputEditText>(R.id.passwordArea)
+
+            Log.d(TAG,email.text.toString())
+            Log.d(TAG,password.text.toString())
+        }
     }
 }
