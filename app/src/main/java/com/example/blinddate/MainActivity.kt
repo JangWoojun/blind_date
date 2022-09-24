d52edd1c1db53562909e34db027199db69ea3b89
JangWoojun <woojun0107@naver.com>
2022-09-24 15:42:27 +0900

    2022/09/24 소개팅 프로필 안에 정보를 회원가입 시 입력한 정보로 넣어주는 기능 구현

--- app/src/main/java/com/example/blinddate/MainActivity.kt
+++ app/src/main/java/com/example/blinddate/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.blinddate
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import com.example.blinddate.auth.IntroActivity
+import com.example.blinddate.auth.UserDataModel
 import com.example.blinddate.slider.CardStackAdapter
+import com.example.blinddate.utils.FirebaseRef
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 import com.yuyakaido.android.cardstackview.CardStackListener
@@ -19,6 +26,9 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var cardStackAdapter: CardStackAdapter // 어뎁터 연결
     lateinit var manager: CardStackLayoutManager // View 를 어떻게 보이는 지 관리 LayoutManager 욕할
+    private val usersDataList = mutableListOf<UserDataModel>()
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,13 +74,31 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        val test = mutableListOf<String>()
-        test.add("a")
-        test.add("b")
-        test.add("c")
 
-        cardStackAdapter = CardStackAdapter(baseContext,test)
+
+        cardStackAdapter = CardStackAdapter(baseContext,usersDataList)
         cardStackView.layoutManager = manager // 매니저에 우리가 만든 CardStackLayoutManager 연결
         cardStackView.adapter = cardStackAdapter //cardStackView에 있는 어댑터에 여기서 만든 cardStackAdapter 연결
+
+        getUserDataList()
+    }
+    private fun getUserDataList(){
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (dataModel in dataSnapshot.children){
+
+                    val user = dataModel.getValue(UserDataModel::class.java)
+                    usersDataList.add(user!!) // 실시간 데이터베이스에서 읽고 거기서 담은 정보를 리스트에 넣어준다
+                }
+                cardStackAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FirebaseRef.userInfoRef.addValueEventListener(postListener)
     }
 }
--- app/src/main/java/com/example/blinddate/slider/CardStackAdapter.kt
+++ app/src/main/java/com/example/blinddate/slider/CardStackAdapter.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.blinddate.R
+import com.example.blinddate.auth.UserDataModel
 
-class CardStackAdapter (val context : Context, val items : List<String>) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>(){
+class CardStackAdapter (val context : Context, val items : List<UserDataModel>) : RecyclerView.Adapter<CardStackAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardStackAdapter.ViewHolder {
         val inflater = LayoutInflater.from(context)
         val view : View = inflater.inflate(R.layout.item_card,parent,false)
@@ -22,7 +24,14 @@ class CardStackAdapter (val context : Context, val items : List<String>) : Recyc
         return items.size
     }
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        fun binding(data : String){
+        val nickname = itemView.findViewById<TextView>(R.id.itemNickname)
+        val age = itemView.findViewById<TextView>(R.id.itemAge)
+        val city = itemView.findViewById<TextView>(R.id.itemCity)
+        // 아이템 카드에 있는 부분을 데이터 안에 들어있는 정보로 교체 여기서 data는 items에 하나하나들이다
+        fun binding(data : UserDataModel){
+            nickname.text = data.nickname
+            age.text = data.age
+            city.text = data.city
 
         }
     }
