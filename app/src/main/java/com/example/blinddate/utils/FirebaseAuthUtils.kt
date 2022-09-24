a7ff60bf1b22ad3ec3113d1f57286229d5d9e7b3
JangWoojun <woojun0107@naver.com>
2022-09-24 14:43:12 +0900

    2022/09/24 파이어베이스 기능 숏컷

---
+++ app/src/main/java/com/example/blinddate/utils/FirebaseAuthUtils.kt
@@ -0,0 +1,16 @@
+package com.example.blinddate.utils
+
+import com.google.firebase.auth.FirebaseAuth
+
+class FirebaseAuthUtils {
+    companion object {
+        private lateinit var auth: FirebaseAuth
+
+        fun getUid():String {
+            auth = FirebaseAuth.getInstance()
+            return auth.currentUser?.uid.toString() // 현재 유저의 uid르 돌려줌
+        }
+
+
+    }
+}
